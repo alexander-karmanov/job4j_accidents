4d6083cb41cecd21e63e0ec695500175416328f9
alexander-karmanov <lektor_md@mail.ru>
2025-10-07 20:48:33 +0300

    0. Spring MVC [#6877]

---
+++ src/main/java/ru/job4j/accidents/controller/IndexController.java
@@ -0,0 +1,17 @@
+package ru.job4j.accidents.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@AllArgsConstructor
+@RequestMapping("/accident")
+public class IndexController {
+    @GetMapping("/index")
+    public String index(Model model) {
+        return "/accident/index";
+    }
+}
