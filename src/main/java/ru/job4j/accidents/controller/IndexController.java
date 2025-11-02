d625ba2efb49c3372b4ad2547bff8b2c4d16f251
alexander-karmanov <lektor_md@mail.ru>
2025-11-02 15:32:34 +0300

    1. Авторизация через JDBC [#2094]

--- src/main/java/ru/job4j/accidents/controller/IndexController.java
+++ src/main/java/ru/job4j/accidents/controller/IndexController.java
@@ -1,10 +1,12 @@
 package ru.job4j.accidents.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.job4j.accidents.model.Accident;
+import ru.job4j.accidents.repository.AccidentHibernate;
 import ru.job4j.accidents.service.AccidentService;
 
 import java.util.List;
@@ -15,10 +17,13 @@ public class IndexController {
 
     private final AccidentService accidentService;
 
-    @GetMapping("/index")
+    private final AccidentHibernate accidents;
+
+
+    @GetMapping("/")
     public String index(Model model) {
-        List<Accident> accidents = accidentService.findAll();
-        model.addAttribute("accidents", accidents);
+        model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        model.addAttribute("accidents", accidentService.findAll());
         return "index";
     }
 }
