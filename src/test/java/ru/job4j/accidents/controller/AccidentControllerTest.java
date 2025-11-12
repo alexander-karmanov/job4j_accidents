9522365501d53ea36b370a081dd92fddd7364970
alexander-karmanov <lektor_md@mail.ru>
2025-11-12 19:19:58 +0300

    0. Spring test [#6881 #554656]

--- src/test/java/ru/job4j/accidents/controller/AccidentControllerTest.java
+++ src/test/java/ru/job4j/accidents/controller/AccidentControllerTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.accidents.controller;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -9,11 +10,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.accidents.Main;
+import ru.job4j.accidents.model.Accident;
+import ru.job4j.accidents.service.AccidentService;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
@@ -21,6 +26,9 @@ public class AccidentControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private AccidentService accidentService;
 
     @Test
     @Transactional
@@ -38,6 +46,9 @@ public class AccidentControllerTest {
     @Transactional
     @WithMockUser
     public void shouldReturnEditAccidentView() throws Exception {
+        Accident testAccident = new Accident();
+        testAccident.setId(1);
+        when(accidentService.getById(1)).thenReturn(Optional.of(testAccident));
         this.mockMvc.perform(get("/formUpdateAccident").param("id", "1"))
                 .andDo(print())
                 .andExpect(status().isOk())
